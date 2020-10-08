fb856adda38b75dd9d90503690f400ef3a1ea0e4
Dikower <dmitriy1d01@gmail.com>
2020-10-08 21:33:12 +0300

    Connecting

---
+++ frontend/src/pages/_components/Checkbox.svelte
@@ -0,0 +1,91 @@
+<script>
+    export let checked = false;
+    export let text = "";
+    export let id = 0;
+</script>
+
+<div class="component">
+    <input id="checkbox-{id}" type="checkbox" class="check">
+    <label class="box" for="checkbox-{id}"></label>
+    <label class="label" for="checkbox-{id}">{@html text}</label>
+</div>
+
+<style>
+
+    .check:not(:checked),
+    .check:checked {
+        display: none;
+    }
+
+    .box {
+        width: 25px;
+        height: 25px;
+        /*margin: 0 20px;*/
+    }
+
+    .check:not(:checked) + .box,
+    .check:checked + .box {
+        position: relative;
+        /*margin: 0 20px;*/
+        cursor: pointer;
+    }
+
+    /* checkbox aspect */
+    .check:not(:checked) + .box:before,
+    .check:checked + .box:before {
+        content: '';
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 20px;
+        height: 20px;
+        border: 2px solid black;
+        background: transparent;
+        transition: all .2s ease;
+
+    }
+
+    .check:checked + .box:before {
+        background: black;
+        box-shadow: black;
+    }
+
+    /* checked mark aspect */
+    .check:not(:checked) + .box:after,
+    .check:checked + .box:after {
+        content: '•';
+        position: absolute;
+        border-radius: 80px;
+        top: 3px;
+        left: 7px;
+        font-size: 23px;
+        line-height: 0.8;
+        background-color: black;
+        color: white;
+        transition: all .3s ease;
+    }
+
+    /* checked mark aspect changes */
+    .check:not(:checked) + .box:after {
+        opacity: 0;
+        transform: scale(0);
+    }
+
+    .check:checked + .box:after {
+        opacity: 1;
+        transform: scale(1);
+    }
+    .label {
+        display: inline-block;
+        cursor: pointer;
+        margin-left: 5px;
+        font-size: calc(var(--plain-font-size) - 2px);
+    }
+    .component {
+        /*margin: 7px;*/
+        display: flex;
+        justify-content: flex-start;
+        align-items: flex-start;
+    }
+
+</style>
---
+++ frontend/src/pages/auth/_reset.svelte
@@ -0,0 +1,29 @@
+<script>
+    import PageTransition from '../_components/PageTransition.svelte';
+</script>
+
+<main>
+<!--    <slot decorator={PageTransition}/>-->
+    <img src="/logo.svg" alt="Common data">
+    <div>
+        <slot/>
+    </div>
+</main>
+
+<style>
+    main {
+        max-width: 1440px;
+        position: absolute;
+        width: 95%;
+        height: 95%;
+        margin-top: 24px;
+    }
+    img {
+        width: 80%;
+    }
+    div {
+        height: 80vh;
+        display: grid;
+        place-items: center;
+    }
+</style>

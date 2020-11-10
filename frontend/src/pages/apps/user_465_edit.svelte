e1ef7373c67a710bb4b5d8191eec4d0f36c9e946
Anastasia Gisina <black_brain.vip@bk.ru>
2020-11-10 21:27:28 +0300

    add front

---
+++ frontend/src/pages/apps/user_465_edit.svelte
@@ -0,0 +1,44 @@
+<script>
+    import ProfileEdit from './_components/Profile_Edit.svelte';
+
+    let data = {
+        user_name: "user",
+        user_surname: "Драгун",
+        user_avatar_link: "/images/user_images/user465.jpg",
+        tags: [
+            {
+                tag_name: "Здоровье",
+                tag_href: "/health"
+            },
+            {
+                tag_name: "Наука",
+                tag_href: "/health"
+            },
+            {
+                tag_name: "Наука",
+                tag_href: "/health"
+            },
+            {
+                tag_name: "Наука",
+                tag_href: "/health"
+            }
+        ],
+        about:'23 y.o. designer from San Francisco',
+        projects: [
+            {
+                title: "Карта ночного неба",
+                project_href: "/project325"
+            },
+            {
+                title: "Влияние проходимого расстояния на здоровье",
+                project_href: "/project"
+            },
+            {
+                title: "Карта ночного неба",
+                project_href: "/project325"
+            }
+        ]
+    }
+</script>
+
+<ProfileEdit {...data}></ProfileEdit>

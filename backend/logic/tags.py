d7e78ad457350d6edd50fd4b99959b56972b645c
Dikower <dmitriy1d01@gmail.com>
2020-11-19 13:35:40 +0300

    Fixed tag fields

--- backend/logic/tags.py
+++ backend/logic/tags.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from tortoise.contrib.pydantic import pydantic_model_creator
 from models import Tag
 from typing import List
@@ -18,3 +18,11 @@ PublicTags = pydantic_model_creator(Tag, exclude=excluded)
 @router.get('/all', response_model=List[PublicTags])
 async def all_tags():
     return await PublicTags.from_queryset(Tag.all())
+
+
+@router.get('/{name}', response_model=PublicTags)
+async def all_tags(name: str):
+    tag = await Tag.get_or_none(name=name)
+    if tag is None:
+        raise HTTPException(404, 'Tag not found')
+    return await PublicTags.from_tortoise_orm(tag)
